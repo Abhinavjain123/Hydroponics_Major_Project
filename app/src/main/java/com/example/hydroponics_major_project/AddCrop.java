6bf4670765c0e663e99bdfec15d07a5aec489f2d
Niket Pathak <niketpathak08@gmail.com>
2023-08-26 21:00:31 +0530

    Add crop java

---
+++ app/src/main/java/com/example/hydroponics_major_project/AddCrop.java
@@ -0,0 +1,18 @@
+package com.example.hydroponics_major_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class AddCrop extends AppCompatActivity {
+
+    Button nextButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_crop);
+
+
+    }
+}
