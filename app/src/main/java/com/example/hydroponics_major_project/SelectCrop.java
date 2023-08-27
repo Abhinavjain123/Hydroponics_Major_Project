5e50056df9b7fa9e2e540f426ccc7caedc1c5e69
SSricode <s21srish@gmail.com>
2023-08-27 12:08:26 +0530

    adding select crop java

---
+++ app/src/main/java/com/example/hydroponics_major_project/SelectCrop.java
@@ -0,0 +1,37 @@
+package com.example.hydroponics_major_project;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+public class SelectCrop extends AppCompatActivity {
+
+    FloatingActionButton addButton;
+    Button nextButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_crop);
+
+        addButton = (FloatingActionButton) findViewById(R.id.add_button);
+        nextButton = (Button) findViewById(R.id.nextButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AddCrop.class);
+                startActivity(i);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), UpdateParameters.class);
+                startActivity(i);
+            }
+        });
+    }
+}
