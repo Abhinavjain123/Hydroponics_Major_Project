1ac29bc9e5c6df027eb5112ab11c5fa6b357b65f
Abhinavjain123 <abhinavsra@gmail.com>
2023-08-28 12:03:03 +0530

    Added Update Parameter backend

---
+++ app/src/main/java/com/example/hydroponics_major_project/UpdateParameters.java
@@ -0,0 +1,14 @@
+package com.example.hydroponics_major_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateParameters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_parameters);
+    }
+}
