2c41b4f8e4b56331070bd35b9817e13c2624c67c
Sanyam-02 <sanyamagrawal7431@gmail.com>
2023-10-07 16:32:02 +0530

    updating strings.xml and removing hardcoded strings

--- app/src/main/java/com/example/hydroponics_major_project/LiveStatus.java
+++ app/src/main/java/com/example/hydroponics_major_project/LiveStatus.java
@@ -1,7 +1,10 @@
 package com.example.hydroponics_major_project;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +15,18 @@ public class LiveStatus extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crop_live_statue);
         String cropName = getIntent().getStringExtra("cropName");
+        Button backButton = (Button) findViewById(R.id.live_status_back);
 
         TextView cropNameTextView = (TextView) findViewById(R.id.cropNameTextView);
         cropNameTextView.setText(cropName);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ScrollActivity.class);
+                startActivity(i);
+            }
+        });
 
         ThingspeakClient client = new ThingspeakClient();
         AsyncTask<String, String, String> a = client.execute("https://api.thingspeak.com/channels/2080835/feeds.json?api_key=SPJO1DKYA6LX64QW&results=1");
