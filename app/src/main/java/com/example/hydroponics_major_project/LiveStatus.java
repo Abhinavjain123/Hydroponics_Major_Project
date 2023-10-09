b0c81cc0367a290acbb0d217b26ad8a3d48da3ba
Sanyam-02 <sanyamagrawal7431@gmail.com>
2023-10-09 16:45:29 +0530

    Firebase changes

--- app/src/main/java/com/example/hydroponics_major_project/LiveStatus.java
+++ app/src/main/java/com/example/hydroponics_major_project/LiveStatus.java
@@ -7,29 +7,168 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LiveStatus extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.crop_live_statue);
+//        String cropName = getIntent().getStringExtra("cropName");
+//        Button backButton = (Button) findViewById(R.id.live_status_back);
+//
+//        TextView cropNameTextView = (TextView) findViewById(R.id.cropNameTextView);
+//        cropNameTextView.setText(cropName);
+//
+//        backButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(getApplicationContext(), ScrollActivity.class);
+//                startActivity(i);
+//            }
+//        });
+//
+//        ThingspeakClient client = new ThingspeakClient();
+//        AsyncTask<String, String, String> a = client.execute("https://api.thingspeak.com/channels/2080835/feeds.json?api_key=SPJO1DKYA6LX64QW&results=1");
+//        System.out.println(a);
+
+    private DatabaseReference humidName;
+    private DatabaseReference cropName;
+    private DatabaseReference lightName;
+    private DatabaseReference nutrientName;
+    private DatabaseReference tempName;
+    private DatabaseReference phName;
+    private DatabaseReference waterName;
+    private TextView textHumid;
+    private TextView textTemp;
+    private TextView textPh;
+    private TextView textWater;
+    private TextView textLight;
+    private TextView textNutrient;
+    private TextView textCrop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crop_live_statue);
-        String cropName = getIntent().getStringExtra("cropName");
-        Button backButton = (Button) findViewById(R.id.live_status_back);
-
-        TextView cropNameTextView = (TextView) findViewById(R.id.cropNameTextView);
-        cropNameTextView.setText(cropName);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
+        textHumid=findViewById(R.id.rhTextView);
+        textTemp=findViewById(R.id.tempTextView);
+        textPh=findViewById(R.id.phTextView);
+        textWater=findViewById(R.id.waterLevelTextView);
+        textLight=findViewById(R.id.lightTextView);
+        textCrop=findViewById(R.id.cropNameTextView);
+        textNutrient=findViewById(R.id.nutrientTextView);
+        humidName= FirebaseDatabase.getInstance().getReference("Sensor").child("humid");
+        lightName= FirebaseDatabase.getInstance().getReference("Sensor").child("light");
+        cropName= FirebaseDatabase.getInstance().getReference("Sensor").child("cropName");
+        nutrientName= FirebaseDatabase.getInstance().getReference("Sensor").child("nutrients");
+        humidName= FirebaseDatabase.getInstance().getReference("Sensor").child("humid");
+        tempName= FirebaseDatabase.getInstance().getReference("Sensor").child("temp");
+        phName= FirebaseDatabase.getInstance().getReference("Sensor").child("ph");
+        waterName= FirebaseDatabase.getInstance().getReference("Sensor").child("water_value");
+        humidName.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), ScrollActivity.class);
-                startActivity(i);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String data=snapshot.getValue().toString();
+                    textHumid.setText(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        tempName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data=snapshot.getValue().toString();
+                textTemp.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+//
+        phName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data=snapshot.getValue().toString();
+                textPh.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
-        ThingspeakClient client = new ThingspeakClient();
-        AsyncTask<String, String, String> a = client.execute("https://api.thingspeak.com/channels/2080835/feeds.json?api_key=SPJO1DKYA6LX64QW&results=1");
-        System.out.println(a);
+        waterName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data=snapshot.getValue().toString();
+                textWater.setText(data);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        cropName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String data=snapshot.getValue().toString();
+                    textCrop.setText(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        lightName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String data=snapshot.getValue().toString();
+                    textLight.setText(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        nutrientName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String data=snapshot.getValue().toString();
+                    textNutrient.setText(data);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hydroponics_major_project/MainActivity.java
+++ app/src/main/java/com/example/hydroponics_major_project/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
         homeButton = (Button) findViewById(R.id.homeButton);
         liveButton = (Button) findViewById(R.id.liveButton);
-        currentButton=(Button) findViewById(R.id.button);
+//        currentButton=(Button) findViewById(R.id.button);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,18 +30,18 @@ public class MainActivity extends AppCompatActivity {
         liveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), ScrollActivity.class);
+                Intent i = new Intent(getApplicationContext(), LiveStatus.class);
                 startActivity(i);
             }
         });
 
-        currentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), read_data.class);
-                startActivity(i);
-            }
-        });
+//        currentButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(getApplicationContext(), read_data.class);
+//                startActivity(i);
+//            }
+//        });
         ThingspeakClient client = new ThingspeakClient();
         client.execute("https://api.thingspeak.com/channels/2080835/feeds.json?api_key=SPJO1DKYA6LX64QW&results=3");
     }
