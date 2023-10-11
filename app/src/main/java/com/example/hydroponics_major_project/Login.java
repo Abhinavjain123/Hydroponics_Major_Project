8df5bc5c2e65320310a91b725441da5fd290f301
SSricode <s21srish@gmail.com>
2023-10-11 15:15:54 +0530

    login + other changes

---
+++ app/src/main/java/com/example/hydroponics_major_project/Login.java
@@ -0,0 +1,49 @@
+package com.example.hydroponics_major_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+    TextView signupText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        signupText = findViewById(R.id.signupText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
+            }
+        });
+    }
+}
