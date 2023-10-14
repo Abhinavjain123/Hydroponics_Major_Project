30318fb8738d44bb7e99b44b04f3de9048a5d475
SSricode <s21srish@gmail.com>
2023-10-14 15:43:39 +0530

    home

---
+++ app/src/main/java/com/example/hydroponics_major_project/Home.java
@@ -0,0 +1,46 @@
+package com.example.hydroponics_major_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.home);
+
+        Button home_loginButton = findViewById(R.id.home_loginButton);
+
+        Button home_registerButton = findViewById(R.id.home_registerButton);
+
+        home_loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+            }
+
+        });
+        home_registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Home.this, Register.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
