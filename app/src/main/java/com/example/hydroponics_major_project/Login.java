0a74775c8c8fc52d8f9e339573a18af6d610f214
SSricode <s21srish@gmail.com>
2023-10-14 16:51:08 +0530

    flow

--- app/src/main/java/com/example/hydroponics_major_project/Login.java
+++ app/src/main/java/com/example/hydroponics_major_project/Login.java
@@ -30,10 +30,15 @@ public class Login extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("123456")) {
                     Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    startActivity(i);
+
                 } else {
                     Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Login.this, Home.class);
+                    startActivity(i);
                 }
             }
         });
