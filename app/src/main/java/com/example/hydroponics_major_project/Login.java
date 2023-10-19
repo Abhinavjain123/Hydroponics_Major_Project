c582b4b76fc2f633a259220933165effaef80892
SSricode <s21srish@gmail.com>
2023-10-19 19:47:58 +0530

    register db login db

--- app/src/main/java/com/example/hydroponics_major_project/Login.java
+++ app/src/main/java/com/example/hydroponics_major_project/Login.java
@@ -10,6 +10,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
@@ -19,10 +25,11 @@ public class Login extends AppCompatActivity {
     Button loginButton;
     TextView signupText;
 
-    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String MyPREFERENCES = "MyPrefs";
     public static final String Username = "username";
     public static final String Password = "password";
     SharedPreferences sharedpreferences;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,35 +42,53 @@ public class Login extends AppCompatActivity {
         signupText = findViewById(R.id.signupText);
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        db = FirebaseFirestore.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String enteredUsername = username.getText().toString();
+                final String enteredPassword = password.getText().toString();
 
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("123456")) {
+               CollectionReference usersCollection = db.collection("users");
+//                Query query = usersCollection.whereEqualTo("userName", enteredUsername);
+//                DocumentReference userDocument = usersCollection.document(enteredUsername);
+                CollectionReference userNameCollection = usersCollection.document("uc3tKaMtqMqZOv5EQMe4").collection(enteredUsername);
+//                Query query = userNameCollection.whereEqualTo("username", enteredUsername);
 
-                    Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    userNameCollection.get().addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            QuerySnapshot querySnapshot = task.getResult();
+                            if (querySnapshot != null && !querySnapshot.isEmpty()) {
+                                DocumentSnapshot document = querySnapshot.getDocuments().get(0);
+                                String storedPassword = document.getString("password");
 
-                    editor.putString(Username, username.getText().toString());
-                    editor.putString(Password, password.getText().toString());
-                    editor.commit();
+                            if (enteredPassword.equals(storedPassword)) {
+                                Toast.makeText(Login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                                SharedPreferences.Editor editor = sharedpreferences.edit();
+                                editor.putString(Username, enteredUsername);
+                                editor.putString(Password, enteredPassword);
+                                editor.commit();
 
-                    Intent i = new Intent(Login.this, MainActivity.class);
-                    startActivity(i);
-
-                } else {
-                    Toast.makeText(Login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(Login.this, Home.class);
-                    startActivity(i);
-                }
+                                Intent i = new Intent(Login.this, MainActivity.class);
+                                startActivity(i);
+                            } else {
+                                Toast.makeText(Login.this, "Login Failed! Incorrect password.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Login.this, "Login Failed! User not found.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(Login.this, "Login Failed! Please try again.", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
         signupText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), Register.class);
+                Intent i = new Intent(Login.this, Register.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/hydroponics_major_project/Register.java
+++ app/src/main/java/com/example/hydroponics_major_project/Register.java
@@ -7,18 +7,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.CollectionReference;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register extends AppCompatActivity {
 
-    private DatabaseReference fullName;
-    private DatabaseReference userName;
-    private DatabaseReference email;
-    private DatabaseReference passwd;
+    private FirebaseFirestore firestore;
+    private CollectionReference usersCollection;
+  //  private DatabaseReference fullName;
+  //  private DatabaseReference userName;
+  //  private DatabaseReference email;
+  //  private DatabaseReference passwd;
     EditText registration_fullname;
     EditText registration_username;
 
@@ -32,17 +38,15 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-
+        firestore = FirebaseFirestore.getInstance();
         registration_fullname = findViewById(R.id.registration_fullname);
         registration_username = findViewById(R.id.registration_username);
         registration_email = findViewById(R.id.registration_email);
         registration_password = findViewById(R.id.registration_password);
         registerButton= findViewById(R.id.registerButton);
         loginRedirectText = findViewById(R.id.loginRedirectText);
-        fullName= FirebaseDatabase.getInstance().getReference("User").child("name");
-        userName= FirebaseDatabase.getInstance().getReference("User").child("userName");
-        email= FirebaseDatabase.getInstance().getReference("User").child("email");
-        passwd= FirebaseDatabase.getInstance().getReference("User").child("password");
+
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,22 +56,31 @@ public class Register extends AppCompatActivity {
                 String email1 = registration_email.getText().toString();
                 String password1 = registration_password.getText().toString();
 
-
-
+                usersCollection = firestore.collection("users")
+                        .document("uc3tKaMtqMqZOv5EQMe4").collection(username1);
                 if (isValidRegistration(fullName1, username1, email1, password1)) {
-                    fullName.setValue(fullName1);
-                    userName.setValue(username1);
-                    email.setValue(email1);
-                    passwd.setValue(password1);
 
-                    Toast.makeText(Register.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(getApplicationContext(), Login.class);
-                    startActivity(i);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("name", fullName1);
+                    user.put("userName", username1);
+                    user.put("email", email1);
+                    user.put("password", password1);
+
+                    usersCollection.add(user)
+                            .addOnSuccessListener(documentReference -> {
+                                Toast.makeText(Register.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(getApplicationContext(), Login.class);
+                                startActivity(i);
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(Register.this, "Registration Failed! Please check your input.", Toast.LENGTH_SHORT).show();
+                            });
                 } else {
                     Toast.makeText(Register.this, "Registration Failed! Please check your input.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
